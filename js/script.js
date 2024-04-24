92238abbaec522d6f79a0b6be463168fe4f9df2a
petrenko@binaryanvil.com <mpetrenko@binaryanvil.com>
2024-04-24 18:52:48 +0300

    41-42 lessons

--- js/script.js
+++ js/script.js
@@ -67,11 +67,120 @@ window.addEventListener("DOMContentLoaded", () => {
 
 		console.log(now.getUTCHours()); */
 
-		console.log(now.setHours(18, 40));
+		/* console.log(now.setHours(18, 40));
 		console.log(now);
-		console.log(now);
+		console.log(now); */
 
 		// data
+
+		// timer
+
+			const deadline = "2026-05-07";
+
+			function getTimeRemaining(endTime) {
+				const t = Date.parse(endTime) - Date.parse(new Date()), // получаем в милисикундах
+				days = Math.floor(t / (1000 * 60 * 60 * 24)), // в сутках милисикунд
+				hours = Math.floor((t / (1000 * 60 * 604) % 24)),
+				min = Math.floor((t / 1000 / 60) % 60),
+				sec = Math.floor((t / 1000) % 60);
+
+					/* console.log(days);
+					console.log(hours);
+					console.log(min);
+					console.log(sec); */
+
+				return {
+					"total" : t,
+					"days": days,
+					"hours": hours,
+					"min": min,
+					"sec": sec
+				}
+			}
+
+			function getZero(num) {
+				if (num >= 0 && num < 10) {
+					return `0${num}`;
+				} else {
+					return num;
+				}
+			}
+
+			function setClock(selector, endTime) {
+				const timer = document.querySelector(selector),
+					days = timer.querySelector("#days"),
+					hours = timer.querySelector("#hours"),
+					minutes = timer.querySelector("#minutes"),
+					seconds = timer.querySelector("#seconds"),
+					timeInterval = setInterval(updateClock, 1000);
+
+					updateClock();
+
+					/* console.log(hours);
+					console.log(days);
+					console.log(minutes);
+					console.log(seconds); */
+
+					function updateClock() {
+						const t = getTimeRemaining(endTime);
+
+						days.innerHTML = getZero(t.days);
+						hours.innerHTML = getZero(t.hours);
+						minutes.innerHTML = getZero(t.min);
+						seconds.innerHTML = getZero(t.sec);
+
+						if (t.total <= 0) {
+							clearInterval(timeInterval);
+						}
+					}
+			}
+
+			setClock(".timer", deadline);
+
+		// Timer
+
+		// 43
+
+		const scrollBox = document.querySelector(".scroll-window");
+		const btnBox = document.querySelector(".wBtn");
+		const btnBox2 = document.querySelector(".wBtn2");
+
+		const width = scrollBox.scrollWidth;
+		const height = scrollBox.scrollHeight;
+
+
+		/* const width = scrollBox.clientWidth;
+		const heght = scrollBox.clientHeight; */
+
+	/* 	const width = scrollBox.offsetWidth;
+		const heght = scrollBox.offsetHeight; */
+
+		/* console.log(width);
+		console.log(height);
+
+		console.log(scrollBox);
+		console.log(btnBox); */
+
+		btnBox.addEventListener("click", () => {
+			scrollBox.style.height = height + "px";
+
+			/* var eeee = height + "px";
+			console.log(height);
+			console.log(eeee); */
+			//scrollBox.style.height =  "20px";
+		});
+
+		btnBox2.addEventListener("click", () => {
+			window.scrollTo(0, 0);
+		});
+
+		//console.log(scrollBox.getBoundingClientRect().top);
+
+		console.log(scrollBox.getBoundingClientRect());
+
+		const style = window.getComputedStyle(scrollBox);
+		console.log(style);
+		// 43
 
 		
 		
